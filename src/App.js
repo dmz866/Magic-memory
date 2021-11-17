89cd2d87ab0843d99a204ed59b828b27fcb5754f
dmurillo <david.murillo@bairesdev.com>
2021-11-17 14:58:54 -0500

    u

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import "./App.css";
+
+const cardImages = [
+  { src: "img/helmet-1.png" },
+  { src: "img/potion-1.png" },
+  { src: "img/ring-1.png" },
+  { src: "img/scroll-1.png" },
+  { src: "img/shield-1.png" },
+  { src: "img/sword-1.png" },
+];
+
+function App() {
+  const [cards, SetCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random - 0.5)
+      .map((card) => {
+        return { ...card, id: Math.random() };
+      });
+
+    SetCards(shuffleCards);
+    setTurns(0);
+  };
+
+  return (
+    <div className="App">
+      <h1>Magic Match</h1>
+      <button onClick={shuffleCards}>New Game</button>
+    </div>
+  );
+}
+
+export default App;
